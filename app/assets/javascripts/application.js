702d65b4c8b3f41861964e37c2abb253356fdffc
musik <860577@gmail.com>
2012-12-09 17:09:54 +0800

    items c

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,7 @@ $(function () {
 		$(this).attr("href",reverse(unescape($(this).attr('data'))))
 		$(this).attr("target","_blank")
 	})
-  $('#contact-note').html('<span class="badge badge-important"> 敏感内容联系</span>0874-3335702 18087409631&nbsp;'+'<span class="badge badge-info">QQ</span><a target="_blank" href="http://sighttp.qq.com/authd?IDKEY=f62e57b812e5ba6bcabb95e781a841ed8edb5efa46c90997">58265826</a>&nbsp;'+'<span class="badge badge-success">Email</span>admin@sdmec.com')
+  $('#contact-note').html('以下为网站联系方式，用于网安/主机商/机房联系处理敏感内容，或其它各界与网站有关的诸如交换链接/意见反馈等用途。<br />此联系方式与本页可能展示的淘宝店铺无关。<br><span class="badge badge-important"> 敏感内容联系</span>0874-3335702 18087409631&nbsp;'+'<span class="badge badge-info">QQ</span><a target="_blank" href="http://sighttp.qq.com/authd?IDKEY=f62e57b812e5ba6bcabb95e781a841ed8edb5efa46c90997">58265826</a>&nbsp;'+'<span class="badge badge-success">Email</span>admin@sdmec.com')
 })
 function loadFull(){
   $('body').html('<iframe frameborder="0" marginheight="0" marginwidth="0" border="0" id="alimamaifrm" name="alimamaifrm" scrolling="no" height="2432px" width="100%" src="http://www.taobao.com/go/chn/tbk_channel/channelcode.php?pid=mm_10894158_2495491_10853963&eventid=101329" ></iframe>').prepend('<div id="bookmark" data-area="top_add_fav">为保证购物安全 <a class="taf_add" target="_self" href="javascript:AddFavorite(window.location.href,document.title)">收藏淘店铺</a> 正品保证，安全购物！ ')
@@ -78,10 +78,12 @@ function convert_shop(nick,context,valueable){
        }
        //console.debug(resp)
        if(typeof(resp.taobaoke_shops) != "undefined"){
+         var respShop=resp.taobaoke_shops.taobaoke_shop[0];
+         $('.shoplink').attr('href',respShop.click_url)
          if(valueable == false){
-           var respShop=resp.taobaoke_shops.taobaoke_shop[0];
-           console.debug(respShop)
-           $('#shoplink').attr('href',respShop.click_url)
+           //var respShop=resp.taobaoke_shops.taobaoke_shop[0];
+           //console.debug(respShop)
+           //$('#shoplink').attr('href',respShop.click_url)
            var args = $('#shoplink').attr('onclick').match(/(\d+),\'(.+?)\'/)
             _gaq.push(['_trackEvent','tovalue',args[2],"http://shop" + args[1] + ".taobao.com"])
             //$('#shoplink').click(function(){
