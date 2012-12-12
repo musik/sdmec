506be7f28f4c28d5070b1e6d36cb4bfa70383ea5
musik <860577@gmail.com>
2012-12-12 17:35:08 +0800

    zzrt

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,7 +7,7 @@ function reverse(s){
     return s.split("").reverse().join("");
 }
 function rl(str){
-	reverse(unescape(str))
+	return reverse(unescape(str));
 }
 function goto(url,o){
 	if ($(o).attr("href") == "javascript://"){
@@ -16,11 +16,25 @@ function goto(url,o){
 		$(o).attr("onclick","")
 	}
 }
+function rewrite_link(str){
+  console.debug(rl(str))
+  $('.shoplink').attr('href',reverse(str))
+}
 $(function () {
+  //$('#full1').html('<div class="container"><<img src="http://img02.taobaocdn.com/tps/i2/T1Gg.bXe0iXXbeBPzh-990-318.jpg"><img src="http://img03.taobaocdn.com/tps/i3/T1yXocXnFjXXaRrOzh-990-171.jpg" /></div>')
 	$('a.rl').click(function(){
 		$(this).attr("href",reverse(unescape($(this).attr('data'))))
 		$(this).attr("target","_blank")
 	})
+  $('.content').before('<div id="slot1"><div>')
+
+  $('#details-section').before('<div id="slotb"></div>')
+  $('#reviews-section').after('<div id="slotf"></div>')
+  $.getScript('http://cbjs.baidu.com/js/m.js',function(){
+    //BAIDU_CLB_fillSlotAsync('503903','full1')
+    BAIDU_CLB_fillSlotAsync('484907','slotb')
+    BAIDU_CLB_fillSlotAsync('484908','slotf')
+  })
   $('#contact-note').html('以下为网站联系方式，用于网安/主机商/机房联系处理敏感内容，或其它各界与网站有关的诸如交换链接/意见反馈等用途。<br />此联系方式与本页可能展示的淘宝店铺无关。<br><span class="badge badge-important"> 敏感内容联系</span>0874-3335702 18087409631&nbsp;'+'<span class="badge badge-info">QQ</span><a target="_blank" href="http://sighttp.qq.com/authd?IDKEY=f62e57b812e5ba6bcabb95e781a841ed8edb5efa46c90997">58265826</a>&nbsp;'+'<span class="badge badge-success">Email</span>admin@sdmec.com')
 })
 function loadFull(){
@@ -29,17 +43,6 @@ function loadFull(){
 function appendFull(){
   return ;
   $('#main').after('<iframe frameborder="0" marginheight="0" marginwidth="0" border="0" id="alimamaifrm" name="alimamaifrm" scrolling="no" height="2432px" width="100%" src="http://www.taobao.com/go/chn/tbk_channel/channelcode.php?pid=mm_10894158_2495491_10853963&eventid=101329" ></iframe>')
-}
-function AddFavorite(sURL, sTitle){
-     try{
-              window.external.addFavorite(sURL, sTitle);
-                 } catch (e){
-                          try{
-                                       window.sidebar.addPanel(sTitle, sURL, "");
-                                              }catch (e){
-                                                           alert("请按Ctrl+D收藏我们");
-                                                                  }
-                             }
 }
 function track_store_click(id,title,act){
   _gaq.push(['_trackEvent',act,title,"http://shop" + String(id) + ".taobao.com"])
@@ -53,6 +56,10 @@ function track_store_visit(id,gaq){
 function track_search_visit(id,gaq){
   gaq.push(['_trackEvent','search','link',id])
 }
+function track_top_ad(){
+  //console.debug(window.location.href)
+  _gaq.push(['_trackEvent','topad','click',window.location.href])
+}
 function link_load(id,link_id){
   $(id).load('/links/' + String(link_id) + '.json',function(){
     $('#mingdian a').click(function(){
@@ -62,9 +69,6 @@ function link_load(id,link_id){
   })
 }
 function convert_shop(nick,context,valueable){
-  //console.debug(document.cookie)
-  //$.post('/xtao.js',function(){
-    //console.debug(document.cookie)
     fields = valueable ? 'click_url,commission_rate' : 'user_id,click_url,commission_rate,seller_credit,shop_type,total_auction,auction_count' 
     TOP.api('rest', 'get',{
       method:'taobao.taobaoke.widget.shops.convert',
@@ -81,22 +85,13 @@ function convert_shop(nick,context,valueable){
          var respShop=resp.taobaoke_shops.taobaoke_shop[0];
          $('.shoplink').attr('href',respShop.click_url)
          if(valueable == false){
-           //var respShop=resp.taobaoke_shops.taobaoke_shop[0];
-           //console.debug(respShop)
-           //$('#shoplink').attr('href',respShop.click_url)
            var args = $('#shoplink').attr('onclick').match(/(\d+),\'(.+?)\'/)
             _gaq.push(['_trackEvent','tovalue',args[2],"http://shop" + args[1] + ".taobao.com"])
-            //$('#shoplink').click(function(){
-              //track_store_click(args[1],args[2],'freetodirect')
-            //})
          }
        }else{
          if(valueable){
            var args = $('#shoplink').attr('onclick').match(/(\d+),\'(.+?)\'/)
             _gaq.push(['_trackEvent','tofree',args[2],"http://shop" + args[1] + ".taobao.com"])
-            //$('#shoplink').click(function(){
-              //track_store_click(args[1],args[2],'tofree')
-            //})
          }
        }
     })  
