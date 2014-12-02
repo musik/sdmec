90a06633bf951c7fe383eeb2e3eb856339468f3e
musik <cgicn@qq.com>
2014-12-02 20:51:53 +0800

    自动检测链接

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Tb::Application.configure do
   config.action_controller.asset_host = "http://www.sdmec.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w(ie6.css ie6.js html5*.js jquery.lazyload.js bootstrap-tab.js home.js inhome_manage.js cat_stores.js)
+  config.assets.precompile += %w(ie6.css ie6.js html5*.js jquery.lazyload.js bootstrap-tab.js home.js inhome_manage.js cat_stores.js entries.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
